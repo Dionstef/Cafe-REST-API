07ece4e046e063a136074bdd51f45fa7f0bc1d9a
Dionisis Stefanitsis <122812246+Dionstef@users.noreply.github.com>
2023-03-14 22:21:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ db = SQLAlchemy(app)
 
 ##Cafe TABLE Configuration
 class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
     
     def to_dict(self):
         dictionary = {}
@@ -31,17 +42,6 @@ def home():
 
 @app.route("/db-init")
 def db_init():
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), unique=True, nullable=False)
-    map_url = db.Column(db.String(500), nullable=False)
-    img_url = db.Column(db.String(500), nullable=False)
-    location = db.Column(db.String(250), nullable=False)
-    seats = db.Column(db.String(250), nullable=False)
-    has_toilet = db.Column(db.Boolean, nullable=False)
-    has_wifi = db.Column(db.Boolean, nullable=False)
-    has_sockets = db.Column(db.Boolean, nullable=False)
-    can_take_calls = db.Column(db.Boolean, nullable=False)
-    coffee_price = db.Column(db.String(250), nullable=True)
     db.create_all()
     return jsonify({"status": "success", "message": "db initialized successfully"})
 
